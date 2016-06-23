d1c6f8490bd2ffeae3cabe91a9c833858aeb9ef0
andela-jngatia <joan.ngatia@andela.com>
2016-06-23 17:38:00 +0300

    Restyle just added categories

--- task3/app.js
+++ task3/app.js
@@ -1,7 +1,7 @@
 var $ = jQuery;
 var main = function (){
     $(".category").click(function(){
-         $("ol").append(" <li>New category</li>.");
+         $("ol").append(" <li><h4>New category<h4></li>.");
         });
 }
 
