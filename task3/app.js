6ff4c50c4d3d4b469652ce58ff93a230b7ac9715
andela-jngatia <joan.ngatia@andela.com>
2016-06-24 10:05:00 +0300

    Add delete button to category elements

--- task3/app.js
+++ task3/app.js
@@ -1,8 +1,22 @@
 var $ = jQuery;
-var main = function (){
-    $(".category").click(function(){
-         $("ol").append(" <li><h4>New category<h4></li>.");
-        });
+var main = function() {
+    // $(".category").click(function() {
+    //     $("ol").append(" <li><h4>New category<h4></li>.");
+    // });
+
+    $('#add').click(function() {
+        var Description = $('#description').val();
+        if ($("#description").val() == '') {
+            $('#alert').html("<strong>Warning!</strong> You left the category empty");
+            $('#alert').fadeIn().delay(1000).fadeOut();
+            return false;
+        }
+        $('.categories').appendpend("<li><h4>" + Description + "</h4></li>");
+        $('#form')[0].reset();
+        var categories = $('.categories').html();
+        localStorage.setItem('categories', categories);
+        // return false;
+    });
 }
 
 $(document).ready(main);
