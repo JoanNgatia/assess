dce92e0b3f60ad43d2697e533209aba4f7105dd3
andela-jngatia <joan.ngatia@andela.com>
2016-06-27 17:46:47 +0300

    Add table definition for the libraries database.

---
+++ task2/book_library.sql
@@ -0,0 +1,7 @@
+-- To retrieve all books with authors from the Table LIBRARY:
+SELECT title, author from Library;
+
+-- To add a new book to the library;
+INSERT INTO Library (title, author, read, date_added)
+VALUES ('Cardinal','Tom B. Erichsen','false','02-11-2015');
+
