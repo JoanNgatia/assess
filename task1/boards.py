957dff70748bf8cbe61c0ec267df71aa4533bd16
andela-jngatia <joan.ngatia@andela.com>
2016-06-27 16:20:02 +0300

    Finish task 1 part a

--- task1/boards.py
+++ task1/boards.py
@@ -1,17 +1,18 @@
 import requests
+# import json
 
 ACCESS_KEY = 'da15af8c50eed328c49a1a416074463a'
-# sample =
-# 'https://api.trello.com/1/boards/4eea4ffc91e31d1746000046/checklists?key=[application_key]&token=[optional_auth_token]'
 
 
-def get_checklists(board_id):
-    # r = requests.get('https://api.github.com/events')
+def get_checklists():
+    board_id = raw_input('Which board do you want to copy from?')
     payload = {'key': 'da15af8c50eed328c49a1a416074463a'}
-    r = requests.get(
-        'https://api.trello.com/1/boards/checklists', params=payload)
-    import ipdb
-    ipdb.set_trace()
-    return r
+    url_to_checklists = 'https://api.trello.com/1/boards/' + board_id + '/checklists'
+    checklist_data = requests.get(url_to_checklists, data=payload)
+    url_to_lists = 'https://api.trello.com/1/boards/' + board_id + '/lists'
+    post_data = {'data': checklist_data}
+    paster = requests.post(url_to_lists, params=payload,
+                           data=post_data)
+    return paster
 
-print get_checklists(20)
+get_checklists()
