84ad167ec6f62f498ffc0c2c29ced2324bb5d7fb
andela-jngatia <joan.ngatia@andela.com>
2016-06-27 17:36:22 +0300

    Finish Task b

--- task1/boards2.py
+++ task1/boards2.py
@@ -24,19 +24,45 @@ class List(object):
                                  data=self.name)
         return response.data
 
-    def edit(self, board_id, name):
+    def edit(self, list_id, name):
         """Edit a board's list."""
-        url_to_lists = 'https://api.trello.com/1/boards/' + self.board_id
-        + '/lists'
+        url_to_lists = 'https://api.trello.com/1/lists/' + self.list_id
         payload = {'key': 'da15af8c50eed328c49a1a416074463a'}
         response = requests.put(url_to_lists, params=payload,
                                 data=self.name)
         return response.data
 
-    def delete(self, board_id):
-        """Edit a board's list."""
+    def delete(self, board_id, list_id):
+        """Delete a board's lists."""
         url_to_lists = 'https://api.trello.com/1/boards/' + self.board_id
-        + '/lists'
+        + '/lists/' + self.list_id
         payload = {'key': 'da15af8c50eed328c49a1a416074463a'}
         response = requests.delete(url_to_lists, params=payload)
+        return response.data
+
+
+class Card(object):
+    """Class model to create ,edit and delete board cards."""
+
+    def create(self, name):
+        """Create a card."""
+        url_to_cards = 'https://api.trello.com/1/cards'
+        payload = {'key': 'da15af8c50eed328c49a1a416074463a'}
+        response = requests.post(url_to_cards, params=payload,
+                                 data=self.name)
+        return response.data
+
+    def edit(self, card_id, name):
+        """Edit a card's name."""
+        url_to_cards = 'https://api.trello.com/1/cards/' + self.card_id
+        payload = {'key': 'da15af8c50eed328c49a1a416074463a'}
+        response = requests.put(url_to_cards, params=payload,
+                                data=self.name)
+        return response.data
+
+    def delete(self, card_id):
+        """Delete a card."""
+        url_to_cards = 'https://api.trello.com/1/cards/' + self.card_id
+        payload = {'key': 'da15af8c50eed328c49a1a416074463a'}
+        response = requests.delete(url_to_cards, params=payload)
         return response.data
