49e5e5a7cb9d28433faddec04dedbc9369aec286
andela-jngatia <joan.ngatia@andela.com>
2016-06-29 11:20:34 +0300

    Add jquery logic to add new categories

--- task3/app.js
+++ task3/app.js
@@ -1,22 +1,17 @@
 var $ = jQuery;
 var main = function() {
-    // $(".category").click(function() {
-    //     $("ol").append(" <li><h4>New category<h4></li>.");
-    // });
 
-    $('#add').click(function() {
-        var Description = $('#description').val();
-        if ($("#description").val() == '') {
-            $('#alert').html("<strong>Warning!</strong> You left the category empty");
-            $('#alert').fadeIn().delay(1000).fadeOut();
-            return false;
-        }
-        $('.categories').appendpend("<li><h4>" + Description + "</h4></li>");
-        $('#form')[0].reset();
-        var categories = $('.categories').html();
-        localStorage.setItem('categories', categories);
-        // return false;
+    $('#add').click( function() {
+      var Description = $('#description').val();
+      console.log(Description)
+      $('.categories').append('<li><a>'+Description+'</a></li>');
+      var categories = $('.categories').html();
+      localStorage.setItem('categories', categories);
     });
+
+    if(localStorage.getItem('categories')){
+      $('.categories').html(localStorage.getItem('categories'));
+    }
 }
 
 $(document).ready(main);
